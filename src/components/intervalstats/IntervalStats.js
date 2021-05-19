c27310af77784e29d9ed8a24ea46904bfbaf0f7b
kbaczkiewicz <k.baczkiewicz@i-systems.pl>
2021-05-19 18:32:41 +0200

    fix interval stats issue

--- src/components/intervalstats/IntervalStats.js
+++ src/components/intervalstats/IntervalStats.js
@@ -1,10 +1,19 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {getInterval} from "../../service/IntervalService";
 import {getIntervalStats} from "../../service/IntervalStatsService";
 
+
+
 export const IntervalStats = props => {
-    const [interval, setInterval] = useState(getInterval(props.match.params.intervalId));
-    const [stats, setStats] = useState(getIntervalStats(props.match.params.intervalId));
+    const [interval, setInterval] = useState([]);
+    const [stats, setStats] = useState([]);
+
+    useEffect(() => {
+        getInterval(props.match.params.intervalId).then((data) => setInterval(data));
+        getIntervalStats(props.match.params.intervalId).then((data) => setStats(data));
+    }, []);
+
+
 
     return (
         <div className='container'>
@@ -17,7 +26,7 @@ export const IntervalStats = props => {
                 <div className='col-6 offset-3'>
                     <p>Ukończone aktywności: <b>{stats.completedAmount}</b></p>
                     <p>Nieukończone aktywności: <b>{stats.failedAmount}</b></p>
-                    <p>% ukończonych aktywności: <b>{stats.completedPercent}</b></p>
+                    <p>% ukończonych aktywności: <b>{(stats.completedPercent ?? 0) * 100}%</b></p>
                 </div>
             </div>
         </div>
--- src/service/IntervalStatsService.js
+++ src/service/IntervalStatsService.js
@@ -3,7 +3,7 @@ import {getClient} from "../client/Client";
 
 export const getIntervalStats = async intervalId => {
     const response = await getClient().getIntervalStats(intervalId);
-
+    console.log(response.data.data.completedAmount);
     return new IntervalStats(
         response.data.data.completedAmount,
         response.data.data.failedAmount,
